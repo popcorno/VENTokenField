221358809022b709b31be80255e662e2520c2b2e
Sergey Bugaev <sergey.bugaev@team.wrike.com>
2019-09-05 15:23:57 +0300

    Fix problem with blinking keyboard

--- VENTokenField/VENTokenField.m
+++ VENTokenField/VENTokenField.m
@@ -201,7 +201,12 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
 {
     [self.collapsedLabel removeFromSuperview];
     BOOL inputFieldShouldBecomeFirstResponder = self.inputTextField.isFirstResponder;
-    [self.scrollView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    for (UIView *view in self.scrollView.subviews) {
+        if ([view isKindOfClass:[VENBackspaceTextField class]] == false) {
+            [view removeFromSuperview];
+        }
+    }
+    
     self.scrollView.hidden = NO;
     [self removeGestureRecognizer:self.tapGestureRecognizer];
 
